5e56f19555f1d52301ccc06cc62e4078c2d3ea66
Apriawati <60224718+Apriawati@users.noreply.github.com>
2020-04-14 16:30:46 +0700

    Add files via upload

--- RandomValue.c
+++ RandomValue.c
@@ -1,93 +1,28 @@
-/* C program to randomly select a node from a singly 
-   linked list */
 #include<stdio.h> 
 #include<stdlib.h> 
-#include <time.h> 
-  
-/* Link list node */
+
+// Fungsi random value agar head gerak dari tempat yang random
 struct Node 
 { 
-    int key; 
-    struct Node* next; 
+    char key[100];
+    char value[100];
+    struct Node *next;
+    struct Node *prev;; 
 }; 
   
-// A reservoir sampling based function to print a 
-// random node from a linked list 
-void printRandom(struct Node *head) 
-{ 
-    // IF list is empty 
+struct node RandomValue(struct Node *head, int n_kata){ 
+    int n_random = (rand() % (n_kata + 1)); // merandom head keberapa
+    struct node *curr_node = head;
+
+    // Jika list kosong
     if (head == NULL) 
        return; 
-  
-    // Use a different seed value so that we don't get 
-    // same result each time we run this program 
-    srand(time(NULL)); 
-  
-    // Initialize result as first node 
-    int result = head->key; 
-  
-    // Iterate from the (k+1)th element to nth element 
-    struct Node *current = head; 
-    int n; 
-    for (n=2; current!=NULL; n++) 
-    { 
-        // change result with probability 1/n 
-        if (rand() % n == 0) 
-           result = current->key; 
-  
-        // Move to next node 
-        current = current->next; 
+    // menginisialisasi node pertama
+    char result = head->key; 
+    int n;
+    // Iterasi head ke head yang telah dirandom
+    for (n=0; n = n_random; n++) { 
+           head = curr_node->next; 
     } 
-  
-    printf("Randomly selected key is %d\n", result); 
-} 
-  
-/* BELOW FUNCTIONS ARE JUST UTILITY TO TEST  */
-  
-/* A utility function to create a new node */
-struct Node *newNode(int new_key) 
-{ 
-    /* allocate node */
-    struct Node* new_node = 
-        (struct Node*) malloc(sizeof(struct Node)); 
-  
-    /* put in the key  */
-    new_node->key  = new_key; 
-    new_node->next =  NULL; 
-  
-    return new_node; 
-} 
-  
-  
-/* A utility function to insert a node at the beginning 
-  of linked list */
-void push(struct Node** head_ref, int new_key) 
-{ 
-    /* allocate node */
-    struct Node* new_node = newNode(new_key); 
-  
-    /* put in the key  */
-    new_node->key  = new_key; 
-  
-    /* link the old list off the new node */
-    new_node->next = (*head_ref); 
-  
-    /* move the head to point to the new node */
-    (*head_ref)    = new_node; 
-} 
-  
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    struct Node *head = NULL; 
-    push(&head, 5); 
-    push(&head, 20); 
-    push(&head, 4); 
-    push(&head, 3); 
-    push(&head, 30); 
-  
-    printRandom(head); 
-  
-    return 0; 
+    return head; 
 } 
