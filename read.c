9033b517e0c8d05c61b19940842ac61da819ea0b
ULTI5565 <57930753+ULTI5565@users.noreply.github.com>
2020-04-15 14:57:48 +0700

    Update read.c

--- read.c
+++ read.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : A4
+* Hari dan Tanggal : Senin, 13 April 2020
+* Asisten (NIM) : Sarah Az Zahra (13216099)
+* Nama File : read.c
+* Deskripsi : Implementasi read text file dan memindahkan isi file ke array dua dimensi
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
