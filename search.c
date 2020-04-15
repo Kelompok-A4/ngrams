b2e80e011938924a878f356b7ee2993b504eac60
ULTI5565 <57930753+ULTI5565@users.noreply.github.com>
2020-04-15 14:59:09 +0700

    Update search.c

--- search.c
+++ search.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : A4
+* Hari dan Tanggal : Selasa, 14 April 2020
+* Asisten (NIM) : Sarah Az Zahra (13216099)
+* Nama File : search.c
+* Deskripsi : Implementasi search character dalam linked list
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -64,10 +73,10 @@ void findList(struct Node* first, struct Node* second, char nama1[20], char nama
     return; 
 } 
 
-/* Checks whether the value x is present in linked list */
+/* Fungsi untuk mencari character dalam linked list*/
 void search(struct Node* head, int x) 
 { 
-    struct Node* current = head;  // Initialize current 
+    struct Node* current = head;  
     while (current != NULL) 
     { 
         if (strcmp(current->data, x)){ 
