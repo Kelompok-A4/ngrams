27e72968d20684855746b9bec712c7e2b0c05f3f
ULTI5565 <57930753+ULTI5565@users.noreply.github.com>
2020-04-17 14:03:03 +0700

    Update random.h

--- Main File dan header/random.h
+++ Main File dan header/random.h
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : A4
+* Hari dan Tanggal : Rabu, 15 April 2020
+* Asisten (NIM) : Sarah Az Zahra (13216099)
+* Nama File : random.h
+* Deskripsi : Header yang berisi fungsi untuk melakukan Randomization dan Output
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
   
